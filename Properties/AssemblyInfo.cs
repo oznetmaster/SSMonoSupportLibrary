26f2a3e2863be34399b2e6be9dc0c8c7b693f821
oznetmaster <ncolvin@nivloc.com>
2018-06-03 07:41:48 +0800

    Update InternalsVisibleTo

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,4 +9,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo ("SSMonoWebUILibrary")]
 [assembly: InternalsVisibleTo ("SSMonoNetworkInformationLibrary")]
+[assembly: InternalsVisibleTo ("SSMonoNetMailLibrary")]
 
